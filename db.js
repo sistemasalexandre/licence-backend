5a43f71d2b36705242d4b146a23792cb2eb7ffe0
sistemasalexandre <sistemas.alexandre@gmail.com>
2025-11-24 13:53:34 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,23 @@
 // server/db.js
-const { Pool } = require("pg");
+const { Pool } = require('pg');
+
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  console.error('FATAL: DATABASE_URL missing');
+  process.exit(1);
+}
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-module.exports = pool;
+pool.on('error', (err) => {
+  console.error('Unexpected PG error', err);
+  process.exit(-1);
+});
+
+// Exporta um objeto com a chave `pool` (routes.js usa const { pool } = require('./db'))
+module.exports = { pool };
