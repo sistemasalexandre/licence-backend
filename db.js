66fec2236fc2a133b767c0576ba30c4db2a6bfe9
sistemasalexandre <sistemas.alexandre@gmail.com>
2025-11-24 13:33:58 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,22 +1,11 @@
 // server/db.js
-// PostgreSQL connection using pg Pool
-const { Pool } = require('pg');
-
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-  console.error('FATAL: DATABASE_URL not set in env');
-  process.exit(1);
-}
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString,
-  // For Supabase use SSL: set DB_SSL=true in environment
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle PostgreSQL client', err);
-  process.exit(-1);
-});
-
-module.exports = { pool };
+module.exports = pool;
